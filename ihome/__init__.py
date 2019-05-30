d8713b57cdbfe2963b486bd909ad8c5e00cb0fdf
susu <31542614@qq.com>
2019-05-30 11:11:50 +0800

    添加日志打印

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+from logging.handlers import RotatingFileHandler
+
 import redis
 from flask import Flask
 from flask_session import Session
@@ -6,11 +8,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from config import config_map, Config
 from ihome import api_1_0
+import logging
 
 ##创建db对象，app创建出来之后自动绑定
 db=SQLAlchemy()
 #创建redis连接对象
 redis_store=None
+## 配置日志信息
+# 设置日志的记录等级
+logging.basicConfig(level=logging.INFO)
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("C:\\Users\\top\\PycharmProjects\\Ihome\\ihome\\logs\\log.log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（flask app使用的）添加日记录器
+logging.getLogger().addHandler(file_log_handler)
+
 #工厂模式创建app对象
 def creat_app(config_name):
     """创建flask应用对象
--- ihome/api_1_0/demo.py
+++ ihome/api_1_0/demo.py
@@ -1,8 +1,11 @@
 
 from . import api
-
+import logging
+from flask import current_app
 @api.route("/index")
 def index():
+    logging.debug("进入首页了")
+    current_app.logger.error("出错了")
     # session[]=""
     # session.get()
     return "index page"
